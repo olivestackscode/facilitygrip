2b39f424a4321fe3adb4719cfa1836b173ba7c68
Jerry Nwobodo <163215884+olivestackscode@users.noreply.github.com>
2024-09-08 08:43:38 +0100

    Create package-lock.js

---
+++ package-lock.js
@@ -0,0 +1,34 @@
+{
+  "name": "Facility Grip",
+  "version": "1.0.0",
+  "lockfileVersion": 2,
+  "requires": true,
+  "dependencies": {
+    "react": {
+      "version": "17.0.2",
+      "resolved": "https://registry.npmjs.org/react/-/react-17.0.2.tgz",
+      "integrity": "sha512-somethinghere",
+      "requires": {
+        "loose-envify": "^1.1.0"
+      }
+    },
+    "react-dom": {
+      "version": "17.0.2",
+      "resolved": "https://registry.npmjs.org/react-dom/-/react-dom-17.0.2.tgz",
+      "integrity": "sha512-somethinghere",
+      "requires": {
+        "scheduler": "^0.20.2"
+      }
+    },
+    "react-scripts": {
+      "version": "5.0.0",
+      "resolved": "https://registry.npmjs.org/react-scripts/-/react-scripts-5.0.0.tgz",
+      "integrity": "sha512-somethinghere"
+    },
+    "babel-eslint": {
+      "version": "10.1.0",
+      "resolved": "https://registry.npmjs.org/babel-eslint/-/babel-eslint-10.1.0.tgz",
+      "integrity": "sha512-somethinghere"
+    }
+  }
+}
