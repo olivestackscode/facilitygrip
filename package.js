89bd4a08b3083f60cba050c5569a179d223272e5
Jerry Nwobodo <163215884+olivestackscode@users.noreply.github.com>
2024-09-08 08:40:41 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,40 @@
+{
+  "name": "facilities-dashboard",
+  "version": "1.0.0",
+  "description": "A React app for taking stock of facilities.",
+  "main": "index.js",
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "keywords": [
+    "react",
+    "facilities",
+    "dashboard",
+    "stock-management"
+  ],
+  "author": "Jerry",
+  "license": "MIT",
+  "dependencies": {
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2",
+    "react-scripts": "^5.0.0"
+  },
+  "devDependencies": {
+    "babel-eslint": "^10.1.0"
+  },
+  "browserslist": {
+    "production": [
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 1 chrome version",
+      "last 1 firefox version",
+      "last 1 safari version"
+    ]
+  }
+}
